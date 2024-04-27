511cfd023633878fa87d2f2f01bd50a4c675b614
okunabook <hookji@hotmail.com>
2024-04-27 20:14:20 +0700

    V3

--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
 
-router.put('/profile/:id')
+// router.put('/profile/:id')
 
 
 
-module.exports = router
+// module.exports = router
