920585f30dc82fb3fd17f516bddcd7ab964d820e
okunabook <hookji@hotmail.com>
2024-05-13 13:12:24 +0700

    V8.2

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const {viewuser,viewallexchange,removeexchange,removeuser} = require('../control
 
 
 router.get('/:id/admin/viewuser',viewuser) //ดูuserทั้งหมด
-router.delete('removeuser/:id',removeuser)//ลบuser และจะลบทุกอย่างที่เกี่ยวกับuser
+router.delete('/removeuser/:id',removeuser)//ลบuser และจะลบทุกอย่างที่เกี่ยวกับuser
 router.get('/:id/admin/viewexchange',viewallexchange)//ดูexchange ทั้งหมด
 router.delete('/:id/admin/removeexchange/:id_exchange',removeexchange) // ลบexchange
 
