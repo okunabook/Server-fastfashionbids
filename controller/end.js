b507f07f7678893c955a9ec6ad3a9b7c245ab40c
okbook <hookji@hotmail.com>
2024-05-31 15:52:53 +0700

    V.10.6

--- controller/end.js
+++ controller/end.js
@@ -1,9 +1,9 @@
 const db = require('../config/db')
 
 
-//i
+//ใช้id_alllist
 // exports.address = async (req, res, next) => {
-//     const { id_store, id_exchange } = req.params; // ตรวจสอบให้แน่ใจว่าได้ id_exchange จาก params ด้วย
+//     const { id_alllist } = req.params; // ตรวจสอบให้แน่ใจว่าได้ id_exchange จาก params ด้วย
 //     try {
 //         // คิวรีข้อมูลร้านค้า
 //         db.query(
@@ -11,8 +11,8 @@ const db = require('../config/db')
 //             FROM list 
 //             INNER JOIN users ON list.id = users.id
 //             INNER JOIN store ON store.id_store = list.id_store
-//             WHERE list.id_store = ? AND list.status = "success"`,
-//             [id_store],
+//             WHERE list.id_alllist = ? AND list.status = "success"`,
+//             [id_alllist],
 //             (err, stored) => {
 //                 if (err) {
 //                     console.log(err);
@@ -26,8 +26,8 @@ const db = require('../config/db')
 //                     FROM list 
 //                     INNER JOIN users ON list.id = users.id
 //                     INNER JOIN exchange ON exchange.id_exchange = list.id_exchange
-//                     WHERE list.id_exchange = ? AND list.status = "success"`,
-//                     [id_exchange],
+//                     WHERE list.id_alllist = ? AND list.status = "success"`,
+//                     [id_alllist],
 //                     (err, exchanged) => {
 //                         if (err) {
 //                             console.log(err);
@@ -51,47 +51,110 @@ const db = require('../config/db')
 //         return next();
 //     }
 // };
+
+// exports.address = async (req, res, next) => {
+//     const { id } = req.params; // รับ id จาก params
+//     try {
+//         // คิวรีข้อมูลผู้ใช้และร้านค้า
+//         db.query(
+//             `SELECT users.*, store.*
+//             FROM list
+//             INNER JOIN users ON list.id = users.id
+//             LEFT JOIN store ON store.id_store = list.id_store
+//             WHERE list.id = ? AND list.status = "success"`,
+//             [id],
+//             (err, stored) => {
+//                 if (err) {
+//                     console.log(err);
+//                     res.json({ status: "error", message: err });
+//                     return next();
+//                 }
+
+//                 // คิวรีข้อมูลการแลกเปลี่ยน
+//                 db.query(
+//                     `SELECT users.*, exchange.*
+//                     FROM list
+//                     INNER JOIN users ON list.id = users.id
+//                     LEFT JOIN exchange ON exchange.id_exchange = list.id_exchange
+//                     WHERE list.id = ? AND list.status = "success"`,
+//                     [id],
+//                     (err, exchanged) => {
+//                         if (err) {
+//                             console.log(err);
+//                             res.json({ status: "error", message: err });
+//                             return next();
+//                         }
+//                         console.log({status: "success",user:stored[0],exchange: exchanged[0] });
+//                         return res.json({status: "success",user:stored[0],exchange: exchanged[0] });
+//                     }
+//                 );
+//             }
+//         );
+//     } catch (error) {
+//         console.log(error);
+//         res.json({ status: 500, message: "Server Error <address>", error: error });
+//         return next();
+//     }
+// };
+
+
 exports.address = async (req, res, next) => {
     const { id } = req.params; // รับ id จาก params
     try {
         // คิวรีข้อมูลผู้ใช้และร้านค้า
         db.query(
-            `SELECT users.*, store.*
-            FROM list
-            INNER JOIN users ON list.id = users.id
-            LEFT JOIN store ON store.id_store = list.id_store
-            WHERE list.id = ? AND list.status = "success"`,
-            [id],
-            (err, stored) => {
+           
+            `select users.*,exchange.id_exchange
+            from users 
+            inner join exchange on users.id = exchange.id
+            where users.id = ? `,[id],
+            (err,result)=>{
                 if (err) {
                     console.log(err);
                     res.json({ status: "error", message: err });
                     return next();
                 }
-
-                // คิวรีข้อมูลการแลกเปลี่ยน
+                if (result[0].id_exchange.length === 0) {
+                    db.query(
+                        `select *
+                        from list 
+                        inner join users on list.id= users.id
+                        where id_exchange = ?`,[result[0].id_exchange],
+                        (err,data)=>{
+                            if (err) {
+                                console.log(err);
+                                res.json({ status: "error", message: err });
+                                return next();
+                            }
+                            res.json({
+                                message: "success",
+                                data: data,
+                                result:result
+                            });
+                        }
+                    )    
+                }
                 db.query(
-                    `SELECT users.*, exchange.*
-                    FROM list
-                    INNER JOIN users ON list.id = users.id
-                    LEFT JOIN exchange ON exchange.id_exchange = list.id_exchange
-                    WHERE list.id = ? AND list.status = "success"`,
-                    [id],
-                    (err, exchanged) => {
+                    `select *
+                    from list 
+                    inner join users on list.id= users.id
+                    where id_exchange = ?`,[result[0].id_exchange],
+                    (err,data)=>{
                         if (err) {
                             console.log(err);
                             res.json({ status: "error", message: err });
                             return next();
                         }
-
-                        // รวมข้อมูลของตัวเองและฝ่ายตรงข้าม
-                        
-
-                        // ส่งผลลัพธ์กลับ
-                        return res.json({status: "success",user:stored[0],exchange: exchanged[0] });
+                        res.json({
+                            message: "success",
+                            data: data,
+                            result:result
+                        });
                     }
-                );
+                )
             }
+
+            
         );
     } catch (error) {
         console.log(error);
@@ -99,7 +162,6 @@ exports.address = async (req, res, next) => {
         return next();
     }
 };
-
 
 
 
