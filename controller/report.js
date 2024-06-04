f2810daeb79f316c7eb0c77f86c494b186b97851
okbook <hookji@hotmail.com>
2024-06-04 18:54:53 +0700

    V.11.2

---
+++ controller/report.js
@@ -0,0 +1,74 @@
+const db = require('../config/db')
+
+
+exports.postreport = async(res,req)=>{
+    const {id_user,id_me} = req.params
+    const {content} = req.body
+    try {
+        db.query`
+        insert into report (id_user,id_me,content) value(?,?,?)
+        `[id_user,id_me,content],
+        (err,result)=>{
+            if (err) {
+                console.log(err);
+                res.json({ status: "error", message: err });
+                return next();
+            }
+            res.json({
+                message: "success",
+                data: result,
+            });
+        }
+        
+
+        
+    } catch (error) {
+        console.log(error);
+        res.json({ status: 500, message: "postreport", error: error });
+        return next();
+    }
+}
+
+exports.getreport = async (req, res, next) => {
+    const { id_user, id_me } = req.params;
+    
+    try {
+        db.query(
+            `SELECT report.*, users.*
+             FROM report
+             INNER JOIN users ON users.id = report.id_user 
+             WHERE report.id_user = ?`, [id_user],
+            (err, userReportResult) => {
+                if (err) {
+                    console.log(err);
+                    res.json({ status: "error", message: err });
+                    return next(err);
+                }
+                
+                db.query(
+                    `SELECT report.*, users.*
+                     FROM report
+                     INNER JOIN users ON users.id = report.id_user 
+                     WHERE report.id_me = ?`, [id_me],
+                    (err, meReportResult) => {
+                        if (err) {
+                            console.log(err);
+                            res.json({ status: "error", message: err });
+                            return next(err);
+                        }
+                        
+                        res.json({ 
+                            status: "success", 
+                            userReport: userReportResult, 
+                            meReport: meReportResult 
+                        });
+                    }
+                );
+            }
+        );
+    } catch (error) {
+        console.log(error);
+        res.json({ status: 500, message: "postreport", error: error });
+        return next(error);
+    }
+};
---
+++ routes/report.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const {postreport,getreport} = require('../controller/report')
+
+router.post('/report/:id_user/:id_me',postreport)
+router.get('/getreport/:id_user/:id_me',getreport)
+
+module.exports = router
